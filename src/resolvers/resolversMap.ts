b7750503beaa537f4f28d718863c9ab7d6d7ff8c
Anartz Mugika <mugan86@gmail.com>
2019-06-12 18:04:34 +0200

    Remove characters constant

--- src/resolvers/resolversMap.ts
+++ src/resolvers/resolversMap.ts
@@ -4,20 +4,6 @@ import mutation from './mutation';
 import subscription from './subscription';
 import type from './type';
 
-export const characters = [
-    {
-        id: '1',
-        name: 'Walter White',
-        actor: 'Bryan Cranston',
-        total_episodes: 62
-    },
-    {
-        id: '2',
-        name: 'Jesse Pinkman',
-        actor: 'Aaron Paul',
-        total_episodes: 62
-    }
-];
 export const votes: any = [];
 
 export const NEW_VOTE = 'NEW_VOTE';
