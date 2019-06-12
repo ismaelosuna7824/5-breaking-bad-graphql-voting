4e707709200ecb8a931284c26f83b4a9bfd9a34f
Anartz Mugika <mugan86@gmail.com>
2019-06-12 08:37:26 +0200

    Construct schema with typeDefs and resolvers

---
+++ src/schema/index.ts
@@ -0,0 +1,12 @@
+import 'graphql-import-node';
+import typeDefs from './schema.graphql';
+import resolvers from './../resolvers/resolversMap';
+import { makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+export default schema;
