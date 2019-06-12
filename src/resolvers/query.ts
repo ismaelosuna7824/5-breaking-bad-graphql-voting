cc4d4f1e5e59b4b8c57565f532e34bc4f060b780
Anartz Mugika <mugan86@gmail.com>
2019-06-12 18:05:04 +0200

    Change getCharacters and getCharacter with characters and character in Query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,14 +1,14 @@
-import { characters } from './resolversMap';
+import { database } from './../data/data.store';
 import { IResolvers } from 'graphql-tools';
 import lodash from 'lodash';
 
 const query : IResolvers = {
     Query: {
-        getCharacters(_: void, __: any): any{
-            return characters;
+        characters(_: void, __: any): any{
+            return database.characters;
         },
-        getCharacter(_: void, { id }): any {
-            const character = lodash.filter(characters, ['id', id])[0];
+        character(_: void, { id }): any {
+            const character = lodash.filter(database.characters, ['id', id])[0];
             return (character !== undefined )? character: {id, name: `Not found ${id} contain character`, actor: '', total_episodes: -1};
         }
     }
