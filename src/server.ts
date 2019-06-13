c6da94dc36fcddb388eae12f2583caf8768fc309
Anartz Mugika <mugan86@gmail.com>
2019-06-13 08:27:19 +0200

    Configure mongodb database connection and add in context with pubsub

--- src/server.ts
+++ src/server.ts
@@ -5,33 +5,50 @@ import cors from 'cors';
 import { createServer } from 'http';
 import express from 'express';
 import schema from './schema';
+import  MongoClient  from 'mongodb' ;
 
-const app = express();
-const pubsub = new PubSub();
-app.use('*', cors());
-app.use(compression());
+async function start() {
+    const app = express();
+    const pubsub = new PubSub();
+    app.use('*', cors());
+    app.use(compression());
 
-// Inicializamos el servidor de Apollo
-const server = new ApolloServer({
-    schema,
-    context: { pubsub },
-    introspection: true // Necesario
-});
-server.applyMiddleware({ app });
+    // Configuración de la base de datos NoSQL
 
-app.get('/', (_, res) => {
-    res.send('Welcome to Breaking Bad vote system in GraphQL');
-});
+    const MONGO_DB = 'mongodb://localhost:27017/breaking-bad-voting-characters';
+    const LOG_COLOR = "\x1b[36m%s\x1b[0m";
+    const client = await MongoClient.connect( MONGO_DB, { useNewUrlParser: true } );
+    const db = client.db();
+    // https://ramlez.com/blog/monitoring-mongodb-connection-status/
+    if(client.isConnected()) {
+        console.log('==========================DATABASE=============================');
+        console.log(`STATUS: ${LOG_COLOR}`, 'ONLINE');
+        console.log(`NAME: ${LOG_COLOR}`, db.databaseName);
+    }
 
-app.get('/playground', expressPlayground({
-    endpoint: '/graphql'
-}));
-const PORT = process.env.PORT || 5005;
-const httpServer = createServer(app);
-server.installSubscriptionHandlers(httpServer);
-httpServer.listen({ port: PORT }, () : void => {
-    console.log('========================SERVER==================================');
-    console.log(`🚀 🚀  GraphQL Server running @ http://localhost:${PORT}${server.graphqlPath} 🚀 🚀 `);
-    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
-    console.log('================================================================');
-});
+    // Inicializamos el servidor de Apollo
+    const server = new ApolloServer({
+        schema,
+        context: { db, pubsub },
+        introspection: true // Necesario
+    });
+    server.applyMiddleware({ app });
+
+    app.get('/', (_, res) => {
+        res.send('Welcome to Breaking Bad vote system in GraphQL');
+    });
+
+    app.get('/playground', expressPlayground({
+        endpoint: '/graphql'
+    }));
+    const PORT = process.env.PORT || 5005;
+    const httpServer = createServer(app);
+    server.installSubscriptionHandlers(httpServer);
+    httpServer.listen({ port: PORT }, () : void => {
+        console.log('========================SERVER==================================');
+        console.log(`🚀 🚀  GraphQL Server running @ http://localhost:${PORT}${server.graphqlPath} 🚀 🚀 `);
+        console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
+        console.log('================================================================');
+    });
+}
+start();
