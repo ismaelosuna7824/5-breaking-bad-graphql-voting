dbccc4527737e3cda97edcb35c6f7d0a37abc720
Anartz Mugika Ledo <mugan86@gmail.com>
2019-06-14 18:44:10 +0200

    Add constants

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+export const COLLECTIONS = {
+    CHARACTERS : 'characters',
+    VOTES: 'votes'
+}
+
+export const PHOTO_URL_PRINCIPAL = 'https://vignette.wikia.nocookie.net/breakingbad/images';
+
+export const NEW_VOTE = 'NEW_VOTE';
