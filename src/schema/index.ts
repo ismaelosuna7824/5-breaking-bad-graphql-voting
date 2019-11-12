a4c945035182a10a7c90683a3862d0bbeffc07ca
Anartz Mugika Ledo <mugan86@gmail.com>
2019-11-12 19:44:16 +0100

    Add modularize schema graphql files

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,9 @@
 import { GraphQLSchema } from "graphql";
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
 import resolvers from './../resolvers/resolversMap';
 import { makeExecutableSchema } from "graphql-tools";
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+const typeDefs = mergeTypes(fileLoader(`${__dirname}/**/*.graphql`), { all: true });// Y quitamos
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
