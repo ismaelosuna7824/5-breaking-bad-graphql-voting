2f080ccb851fd831bf7cbd096060324bc4329c51
Anartz Mugika Ledo <mugan86@gmail.com>
2019-11-17 19:04:39 +0100

    Modularize schema finish

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,9 +1,9 @@
 import { GraphQLSchema } from "graphql";
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
 import resolvers from './../resolvers/resolversMap';
 import { makeExecutableSchema } from "graphql-tools";
-
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+const typeDefs = mergeTypes(fileLoader(`${__dirname}/**/*.graphql`), {all: true});
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
     resolvers
--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import Database from './config/database';
 import expressPlayground from 'graphql-playground-middleware-express';
 if (process.env.NODE_ENV !== 'production') {
     const envs = environments;
-    console.log(envs);
+    // console.log(envs);
 }
 
 async function init() {
